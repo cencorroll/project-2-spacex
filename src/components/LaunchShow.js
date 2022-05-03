32397d81cb13bedd2e1f7c136cbef5f5ea41c63b
Matthew Bailey <mattbailey1861@gmail.com>
2022-05-03 18:12:30 +0100

    m

--- src/components/LaunchShow.js
+++ src/components/LaunchShow.js
@@ -47,7 +47,7 @@ const LaunchShow = () => {
                 <iframe width="560" height="315" src= {`https://www.youtube.com/embed/${launch.links.youtube_id}`}  title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
               </Col>
               <Col md="6">
-                <p>SUCCESS</p>
+                <p>{launch.success ? 'Successful' : 'Unsuccesful'}</p>
                 <hr />
                 <p>{launch.details}</p>
                 <hr />
